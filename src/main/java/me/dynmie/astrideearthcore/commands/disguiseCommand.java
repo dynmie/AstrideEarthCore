45d3d0e32eb1c3963ec295c4491624cccfaec482
dynmie <xbottam@gmail.com>
2020-09-01 15:17:18 -0700

    Fixed disguiseCommand importing useless things.

--- src/main/java/me/dynmie/astrideearthcore/commands/disguiseCommand.java
+++ src/main/java/me/dynmie/astrideearthcore/commands/disguiseCommand.java
@@ -7,8 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import javax.swing.*;
-
 import static org.bukkit.Bukkit.getPlayer;
 
 public class disguiseCommand implements CommandExecutor {
