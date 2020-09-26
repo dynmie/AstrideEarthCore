2033f5203e5cdd165c518ec859d3e1d3af0e0fe3
dynmie <xbottam@gmail.com>
2020-09-26 15:24:04 -0700

    Added set fly for another player

--- src/main/java/me/dynmie/astrideearthcore/commands/flyCommand.java
+++ src/main/java/me/dynmie/astrideearthcore/commands/flyCommand.java
@@ -8,6 +8,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static org.bukkit.Bukkit.getPlayer;
+
 
 public class flyCommand implements CommandExecutor {
     Main plugin;
@@ -17,28 +19,38 @@ public class flyCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(Utils.chat("&cOnly players can execute this command!"));
-            return false;
+            sender.sendMessage(Utils.chat(plugin.getConfig().getString("console")));
+            return true;
         }
         Player player = (Player) sender;
         if (!(player.hasPermission("astride.fly"))) {
             player.sendMessage(Utils.chat(plugin.getConfig().getString("noperms")));
-            return false;
-        }
-        if (!(player.getAllowFlight())) {
-            player.setAllowFlight(true);
-            player.setFlying(true);
-            player.sendMessage(Utils.chat("&bFlying &lenabled&a."));
-        } else {
-            player.setAllowFlight(false);
-            player.setFlying(false);
-            player.sendMessage(Utils.chat("&cFlying &ldisabled&c."));
+            return true;
         }
 
+        if (!(args.length == 1)) {
+            if (!(player.getAllowFlight())) {
+                player.setAllowFlight(true);
+                player.sendMessage(Utils.chat("&bFlight Mode for " + player.getDisplayName() + "&lenabled&a."));
+            } else {
+                player.setAllowFlight(false);
+                player.sendMessage(Utils.chat("&cFlight Mode for " + player.getDisplayName() + "&ldisabled&c."));
+            }
+        } else {
+            Player target = getPlayer(args[0]);
+            if (target == null) {
+                player.sendMessage(Utils.chat(plugin.getConfig().getString("PlayerOffline")));
+            } else {
+                if (target.getAllowFlight()) {
+                    target.setAllowFlight(false);
+                    player.sendMessage(Utils.chat("&cFlight Mode for " + target.getDisplayName() + "&ldisabled&c."));
+                } else {
+                    target.setAllowFlight(true);
+                    player.sendMessage(Utils.chat("&bFlight Mode for " + target.getDisplayName() + "&lenabled&a."));
+                }
+                return true;
+            }
+        }
         return true;
     }
 }
-
-
-
-//player.sendMessage(Utils.chat(plugin.getConfig().getString("noperms")));
